8a95be9f7bc83f761f9718baeca46761e53395c4
RomainCrispini <romain.crispini@gmail.com>
2020-04-21 18:19:44 +0200

    Nettoyage

--- app/src/main/java/com/romain/cellarv1/outils/CellarPageAdapter.java
+++ app/src/main/java/com/romain/cellarv1/outils/CellarPageAdapter.java
@@ -6,41 +6,42 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class CellarPageAdapter extends FragmentPagerAdapter {
 
-    private ArrayList<Fragment> fragmentArrayList;
-    private ArrayList<String> fragmentTitleArrayList;
+    private List<Fragment> fragments = new ArrayList<>();
+    private List<String> fragmentTitle = new ArrayList<>();
 
     public CellarPageAdapter(FragmentManager fm, int behavior) {
         super(fm, behavior);
-        fragmentArrayList = new ArrayList<>();
-        fragmentTitleArrayList = new ArrayList<>();
     }
 
     @NonNull
     @Override
     public Fragment getItem(int position) {
-        return fragmentArrayList.get(position);
+        return fragments.get(position);
     }
 
     @Override
     public int getCount() {
-        return fragmentArrayList.size();
+        return fragments.size();
     }
 
     public void addFragment(Fragment fragment, String title) {
-        fragmentArrayList.add(fragment);
-        fragmentTitleArrayList.add(title);
+        fragments.add(fragment);
+        fragmentTitle.add(title);
     }
 
     // This method is precaution for androidX, while tabs text not displaying android 9+
     @Nullable
     @Override
     public CharSequence getPageTitle(int position) {
-        return fragmentTitleArrayList.get(position);
+        return null;
+        //return fragmentTitle.get(position);
     }
+
 
 
 }
--- app/src/main/java/com/romain/cellarv1/vue/CellarActivity.java
+++ app/src/main/java/com/romain/cellarv1/vue/CellarActivity.java
@@ -3,13 +3,13 @@ package com.romain.cellarv1.vue;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
-
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.animation.OvershootInterpolator;
 import android.widget.FrameLayout;
-
 import com.google.android.material.tabs.TabLayout;
 import com.romain.cellarv1.R;
 import com.romain.cellarv1.outils.CellarPageAdapter;
@@ -36,33 +36,12 @@ public class CellarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cellar);
         init();
-        initTabs();
-        //loadWineBottleInListView();
-
-
-
-
-
-
-
-
 
     }
-
-
 
     private void init() {
 
         //txtViewBDD.setMovementMethod(new ScrollingMovementMethod()); // Méthode qui rend la textView scrollable
-
-        /*
-        accesLocal = new AccesLocal(this);
-        List<WineBottle> wineBottleList = accesLocal.recoverWineBottleList();
-        for (int i = 0; i < wineBottleList.size(); i++) {
-            txtViewBDD.setText(wineBottleList.toString());
-        }
-
-         */
 
         initCurvedNavigationView();
 
@@ -74,27 +53,53 @@ public class CellarActivity extends AppCompatActivity {
         sortMenu.setTranslationY(200f);
         sortMenu.animate().translationY(0f).setInterpolator(interpolator).setDuration(1500).start();
 
+        initTabs();
 
     }
 
-
     private void initTabs() {
         ViewPager viewPager = (ViewPager) findViewById(R.id.cellarViewPager);
-        TabLayout cellarTabLayout = (TabLayout) findViewById(R.id.cellarTabLayout);
+        final TabLayout cellarTabLayout = (TabLayout) findViewById(R.id.cellarTabLayout);
 
         cellarListFragment = new CellarListFragment();
         cellarStatsFragment = new CellarStatsFragment();
 
-        cellarTabLayout.setupWithViewPager(viewPager);
 
+        cellarTabLayout.setupWithViewPager(viewPager);
         CellarPageAdapter cellarPageAdapter = new CellarPageAdapter(getSupportFragmentManager(), 0);
+
+        viewPager.setPageTransformer(true, new CellarTabsTransition());
 
         cellarPageAdapter.addFragment(cellarListFragment, "List");
         cellarPageAdapter.addFragment(cellarStatsFragment, "Stats");
 
-        viewPager.setPageTransformer(true, new CellarTabsTransition());
-
         viewPager.setAdapter(cellarPageAdapter);
+
+        cellarTabLayout.getTabAt(0).setIcon(R.drawable.ic_euro_symbol_black_24dp);
+        cellarTabLayout.getTabAt(1).setIcon(R.drawable.ic_euro_symbol_black_24dp);
+
+        cellarTabLayout.getTabAt(0).getIcon().setColorFilter(getResources().getColor(android.R.color.black), PorterDuff.Mode.SRC_IN);
+        cellarTabLayout.getTabAt(1).getIcon().setColorFilter(getResources().getColor(android.R.color.white), PorterDuff.Mode.SRC_IN);
+
+
+        cellarTabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                //cellarTabLayout.getTabAt(0).getIcon().setColorFilter(getResources().getColor(android.R.color.black), PorterDuff.Mode.SRC_IN);
+                tab.getIcon().setColorFilter(Color.GREEN, PorterDuff.Mode.SRC_IN);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                //cellarTabLayout.getTabAt(1).getIcon().setColorFilter(getResources().getColor(android.R.color.white), PorterDuff.Mode.SRC_IN);
+                tab.getIcon().setColorFilter(Color.parseColor("#a8a8a8"), PorterDuff.Mode.SRC_IN);
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+
     }
 
     private void initCurvedNavigationView() {
@@ -132,8 +137,5 @@ public class CellarActivity extends AppCompatActivity {
             }
         });
     }
-
-
-
 
 }
